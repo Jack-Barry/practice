0e0741e72968b938daf17694c02aec01ae1bf8c8
Jack Barry <jack.barry@flashparking.com>
2018-10-02 11:05:22 -0500

    Function parameters practice - default values

--- __input__/first.ts
+++ __input__/first.ts
@@ -1,24 +1,11 @@
 /*
-A rest parameter allows us to use ... notation to pass in an array of 
-parameters. The number[] type declaration specifies our type to be an array 
-of numbers
+We can set default values for parameters
 */
 
-const addNums = (...nums: number[]) => {
-  let i: number
-  let sum: number = 0
-
-  for (i = 0; i < nums.length; i++) {
-    sum = sum + nums[i]
-  }
-
-  console.log(`Sum: ${sum}`)
+const calculateDiscount = (price: number, rate: number = 0.5) => {
+  const discount = price * rate
+  console.log(`Discount: ${discount}`)
 }
 
-addNums(1, 2, 3, 4)
-addNums(10, 20, 30, 40)
-
-/* 
-This would throw an error because we're trying to assign a string in the array
-addNums('stuff', 3, 7)
-*/
+calculateDiscount(100)
+calculateDiscount(100, 0.3)
--- __output__/first.js
+++ __output__/first.js
@@ -1,23 +1,10 @@
 /*
-A rest parameter allows us to use ... notation to pass in an array of
-parameters. The number[] type declaration specifies our type to be an array
-of numbers
+We can set default values for parameters
 */
-var addNums = function () {
-    var nums = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        nums[_i] = arguments[_i];
-    }
-    var i;
-    var sum = 0;
-    for (i = 0; i < nums.length; i++) {
-        sum = sum + nums[i];
-    }
-    console.log("Sum: " + sum);
+var calculateDiscount = function (price, rate) {
+    if (rate === void 0) { rate = 0.5; }
+    var discount = price * rate;
+    console.log("Discount: " + discount);
 };
-addNums(1, 2, 3, 4);
-addNums(10, 20, 30, 40);
-/*
-This would throw an error because we're trying to assign a string in the array
-addNums('stuff', 3, 7)
-*/
+calculateDiscount(100);
+calculateDiscount(100, 0.3);
