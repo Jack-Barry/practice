6347f99a0590a03c3874927be93c18edb72887de
Jack Barry <jack.barry@flashparking.com>
2018-10-02 11:17:08 -0500

    Function prototyping

--- __input__/first.ts
+++ __input__/first.ts
@@ -1,8 +1,17 @@
-/*
-Anonymous recursive function calls itself
-*/
+function employee(id: number, name: string) {
+  this.id = id
+  this.name = name
+}
 
-;(() => {
-  const x: string = 'Hello'
-  console.log(`${x} world`)
-})()
+const emp1 = new employee(123, 'Bobby')
+employee.prototype.email = 'smitty@thing.net'
+
+console.log(`Number: ${emp1.id}`)
+console.log(`Name:   ${emp1.name}`)
+console.log(`Email:  ${emp1.email}`)
+
+const emp2 = new employee(124, 'Billy')
+
+console.log(`Number: ${emp2.id}`)
+console.log(`Name:   ${emp2.name}`)
+console.log(`Email:  ${emp2.email}`)
--- __output__/first.js
+++ __output__/first.js
@@ -1,8 +1,13 @@
-/*
-Anonymous recursive function calls itself
-*/
-;
-(function () {
-    var x = 'Hello';
-    console.log(x + " world");
-})();
+function employee(id, name) {
+    this.id = id;
+    this.name = name;
+}
+var emp1 = new employee(123, 'Bobby');
+employee.prototype.email = 'smitty@thing.net';
+console.log("Number: " + emp1.id);
+console.log("Name:   " + emp1.name);
+console.log("Email:  " + emp1.email);
+var emp2 = new employee(124, 'Billy');
+console.log("Number: " + emp2.id);
+console.log("Name:   " + emp2.name);
+console.log("Email:  " + emp2.email);
