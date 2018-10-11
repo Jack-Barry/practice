97e99ba5f6563d5a7e7a06fe895b2d1807c2a04c
Jack Barry <jack.barry@flashparking.com>
2018-10-11 10:47:05 -0500

    Move program instructions to where they belong

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 
-const globals = require("./lib/globals");
-const globalPaths = new globals.Paths();
-
-console.log(globalPaths.workingDir);
-console.log(globalPaths.configPath);
-console.log(globalPaths.moduleRoot);
-console.log(globalPaths.callingDir);
+require("./lib");
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as globals from "./globals";
+
+const globalPaths = new globals.Paths();
+
+console.log(globalPaths.workingDir);
+console.log(globalPaths.configPath);
+console.log(globalPaths.moduleRoot);
+console.log(globalPaths.callingDir);
