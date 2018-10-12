6b89fb3bfb27ec27afc22bd75f15e779388f2264
Jack Barry <jack.barry@flashparking.com>
2018-10-12 08:06:26 -0500

    ReadonlyArray example

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,2 @@
-interface Point {
-    readonly x: number;
-    readonly y: number;
-}
-declare let p1: Point;
-/**
- * Error, cannot overrwite a readonly attribute. It can only be assigned
- * at initialization
- *
- * p1.x = 5
- *
- * */
+declare let a: number[];
+declare let ro: ReadonlyArray<number>;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Point {
-  readonly x: number
-  readonly y: number
-}
-
-let p1: Point = { x: 10, y: 20 }
+let a: number[] = [1, 2, 3, 4]
+let ro: ReadonlyArray<number> = a
 
 /**
- * Error, cannot overrwite a readonly attribute. It can only be assigned
- * at initialization
- *
- * p1.x = 5
- *
- * */
+ * Error, ReadonlyArray does not allow updating of values
+ * ro[0] = 12
+ * ro.push(5)
+ * ro.length = 100
+ * a = ro
+ */
+
+/**
+ * The following is allowed by overriding with a type assertion
+ */
+a = ro as number[]
