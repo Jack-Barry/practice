a73633ab28263acaf9b0df15afa894639fafb2ea
Jack Barry <jack.barry@flashparking.com>
2018-10-12 08:55:47 -0500

    Make it more modular

---
+++ lib/Configurator/ConfigObject/ConfigObject.d.ts
@@ -0,0 +1,7 @@
+export interface IConfigObject {
+    tools?: Array<any>;
+}
+export declare class ConfigObject implements IConfigObject {
+    tools?: Array<any>;
+    constructor(config?: IConfigObject);
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { jsonify } from './globals'
+import { config } from './defaultConfig'
+
+console.log(jsonify(config))
