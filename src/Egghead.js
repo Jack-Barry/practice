97ff8a0fb19dcf22d38321720b9adf10207eef67
Jack Barry <jack.barry@flashparking.com>
2018-10-15 08:38:33 -0500

    Class keyword

--- src/Egghead.js
+++ src/Egghead.js
@@ -1,24 +1,18 @@
-const parent = {
-  hair: 'brown',
-  heightInInches() {
-    return this.height * 12
+class Vehicle {
+  isLegal() {
+    return true
   }
 }
 
-// Using Object.create uses the prototype to inherit from
-const childA = Object.create(parent)
+class Car extends Vehicle {
+  canBeUsed() {
+    return this.isLegal()
+  }
+}
 
-// Using Object.assign creates a new object entirely
-const childB = Object.assign({ height: 5 }, parent)
+const myCar = new Car()
 
-childA.height = 6
-
-console.log(childA.heightInInches())
-console.log(childB.heightInInches())
-
-// This changes the prototype, so the object created using Object.create is
-// changed, but the object created using Object.assign is not.
-parent.heightInInches = () => true
-
-console.log(childA.heightInInches())
-console.log(childB.heightInInches())
+console.log(Object.getPrototypeOf(myCar) === Car.prototype)
+console.log(Object.getPrototypeOf(myCar) === Vehicle.prototype)
+console.log(Object.getPrototypeOf(Car.prototype) === Vehicle.prototype)
+console.log(Vehicle.prototype.isLegal())
