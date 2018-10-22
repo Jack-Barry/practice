f41f7f15cba6b4469e0d16c9b26cabefbdf2c4a3
Jack-Barry <jackaldenbarry@gmail.com>
2018-10-22 17:39:09 -0500

    Clean lib

--- lib/Parser/ParserOutput/ParserOutput.d.ts
+++
@@ -1,9 +0,0 @@
-import { ToolObject } from '../../Configurator';
-interface IParserOutput {
-    [key: string]: boolean | string | Array<string>;
-}
-export declare class ParserOutput implements IParserOutput {
-    [key: string]: boolean | string | Array<string>;
-    constructor(tool: ToolObject, inputs?: Array<string>);
-}
-export {};
